08f0d2bc0d24aeb6f9cffe454c932ce6f57d4b00
Theodoros Anagnostopoulos <theodoros.anagnostopoulos@ibm.com>
2023-07-21 12:18:38 +0300

    test:

--- weather/views.py
+++ weather/views.py
@@ -6,5 +6,6 @@ def getWeather(request):
     city = "Athens"
     url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + "cbec624ef2c60055a21f567c45b3fd47"
     response = requests.get(url).json()
+    icon = "https://openweathermap.org/img/w/" + response['weather'][0]['icon'] + ".png"
     print(response)
-    return  
+    return render(request, 'index2.html',{'city': response['name'], 'weather': response['weather'][0]['main'], 'icon': icon, 'temp': response['main']['temp']})
